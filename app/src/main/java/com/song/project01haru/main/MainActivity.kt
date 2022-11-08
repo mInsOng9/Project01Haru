4317fe248dcfc16e6ce8f3cab86f3acde7edf2e8
song <minns2992@gmail.com>
2022-11-08 17:56:35 +0900

    commit

--- app/src/main/java/com/song/project01haru/main/MainActivity.kt
+++ app/src/main/java/com/song/project01haru/main/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.song.project01haru.main
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.view.MotionEvent
 import android.view.View
-import android.widget.CalendarView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
-
+import com.shuhart.materialcalendarview.CalendarDay
 import com.shuhart.materialcalendarview.MaterialCalendarView
+import com.shuhart.materialcalendarview.MaterialCalendarView.Companion.SELECTION_MODE_MULTIPLE
+import com.shuhart.materialcalendarview.MaterialCalendarView.Companion.SELECTION_MODE_RANGE
 import com.song.project01haru.*
 import com.song.project01haru.databinding.*
 import com.song.project01haru.edit.EditActivity
@@ -27,9 +31,9 @@ import com.song.project01haru.main.expinc.ExpIncFragment
 import com.song.project01haru.main.home.HomeFragment
 import com.song.project01haru.main.skd.SkdFragment
 import com.song.project01haru.main.todo.TodoFragment
+import org.w3c.dom.Text
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,11 +48,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var fragmentManager:FragmentManager
 
     //abt calendar view
-    val date= Date()
-    lateinit var day:String
+    var date= Date()
     val calendar by lazy{binding.calendarView}
+    lateinit var day:CalendarDay
    // val calendar by lazy {binding.calendarView}
     val sdf by lazy {SimpleDateFormat("yyyy.MM")}
+
+    //dialog calendar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,6 +65,9 @@ class MainActivity : AppCompatActivity() {
         val toolbar: androidx.appcompat.widget.Toolbar =binding.toolbar
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        //Title(date)
+        binding.tvTitleDate.setOnClickListener { calDialog() }
 
         //drawerlayout
         val drawerLayout:DrawerLayout=binding.drawerLayout
@@ -104,7 +113,8 @@ class MainActivity : AppCompatActivity() {
         fragmentManager=supportFragmentManager
         fragmentManager.beginTransaction().add(R.id.frag_container,fragments[0]!!).commit()
 
-        //bottom navigation view
+        //bottom navigation view (fragment)
+        // https://velog.io/@jinny_0422/Android-Fragment-Activity%EA%B0%84-%EB%8D%B0%EC%9D%B4%ED%84%B0%EC%A0%84%EB%8B%AC
         bnv.setOnItemSelectedListener {it->
             val tran:FragmentTransaction=fragmentManager.beginTransaction()
 
@@ -195,31 +205,55 @@ class MainActivity : AppCompatActivity() {
         //simpleDateFormat.format(date);
         //day+1year== endday
         // date.setTime(date.getTime()+1000*60*60*24*365);
-        date.time=date.time
-        calendar.monthIndicatorVisible=false
-        calendar.showOtherDates=0
-        calendar.selectionMode=1
-        calView()
 
+        calendar.monthIndicatorVisible=false
+        calendar.selectionMode= SELECTION_MODE_RANGE
+
+        calendar.selectedDates
+        //selectedDates
+       // day=CalendarDay()
+       // day=sdf.format()
+
+
+        fun MaterialCalendarView.setWeekDayFormatter(formatter: SimpleDateFormat) {
+            formatter.format("E")
+        }
+        binding.calendarView.setOnClickListener {
+            fun MaterialCalendarView.addOnRangeSelectedListener(listener: () -> Unit) {
+                calendar.addOnRangeSelectedListener() {
+                    Toast.makeText(this@MainActivity, "asd", Toast.LENGTH_SHORT).show()
+                    binding.tvTitleDate.text =
+                        binding.calendarView.selectedDates?.get(0).year.toString() + "." +
+                                binding.calendarView.selectedDates?.get(0).month.toString()
+                }
+            }
+        }
     }
 
-
-    fun calView(){
-//        calendar.setOnDateChangeListener({ calendarView: CalendarView, i1: Int, i2: Int, i3: Int ->
-//            date.year=i1-1900
-//            date.month=i2
-//            date.date=i3
-//
-//            var tvTitleDate=binding.tvTitleDate
-//            tvTitleDate.text=sdf.format(date)
-//
-//
-//        })
-    }//clView
 
     //to hide fab
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         return super.onTouchEvent(event)
     }
 
+    fun calDialog(){
+        val dialog : AlertDialog=AlertDialog.Builder(this)
+            .setView(R.layout.dialog_date).create()
+        dialog.show()
+
+        val tvOk:TextView?=dialog.findViewById<TextView>(R.id.tv_ok)
+        val tvCancel:TextView?=dialog.findViewById(R.id.tv_cancel)
+        val calendarView=dialog.findViewById<MaterialCalendarView>(R.id.calDialog)
+
+        tvOk?.setOnClickListener {
+            binding.tvTitleDate.text=calendarView?.selectedDate?.year.toString()+"."+calendarView?.selectedDate?.month.toString()
+            dialog.dismiss()
+        }
+
+        tvCancel?.setOnClickListener { dialog.dismiss() }
+
+    }//calDialog
 }
+
+
+
